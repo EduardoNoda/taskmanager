29b993f2d1f22a7e5b6997a3dfe0dba830994229
Eduardo Noda <duba.noda737@gmail.com>
2025-09-13 20:17:55 -0400

    first commit

---
+++ src/main/java/br/com/taskmanager/taskmanager/model/RoleModel.java
@@ -0,0 +1,6 @@
+package br.com.taskmanager.taskmanager.model;
+
+public enum RoleModel {
+    ROLE_USER,
+    ROLE_ADMIN
+}
---
+++ src/main/java/br/com/taskmanager/taskmanager/model/UserModel.java
@@ -0,0 +1,16 @@
+package br.com.taskmanager.taskmanager.model;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class UserModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String cpf;
+    private RoleModel role;
+
+}
