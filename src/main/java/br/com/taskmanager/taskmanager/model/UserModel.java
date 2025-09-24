9944ba1c49aa5403537323a30878b4fc87b495b7
Eduardo Noda <duba.noda737@gmail.com>
2025-09-23 22:28:29 -0400

    chore: change entity UserModel with id, name, password, email, cpf and role

--- src/main/java/br/com/taskmanager/taskmanager/model/UserModel.java
+++ src/main/java/br/com/taskmanager/taskmanager/model/UserModel.java
@@ -10,6 +10,8 @@ public class UserModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
+    private String password;
+    private String email;
     private String cpf;
     private RoleModel role;
 
